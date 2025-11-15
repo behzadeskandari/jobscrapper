79383005bc7361bf4cdbf0e78f4f980041f5339e
105624033 <behzad.b.i.g@gmail.com>
2025-11-15 12:45:29 +0330

    cmt models

---
+++ jobscrapper/DataPipeline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace jobscrapper
+{
+    public class DataPipeline
+    {
+        public static List<Job> LoadJobs(string folder = "Data")
+        {
+            var jobs = new List<Job>();
+            foreach (var file in Directory.GetFiles(folder, "jobs_*.jsonl"))
+            {
+                var lines = File.ReadAllLines(file, Encoding.UTF8);
+                foreach (var line in lines)
+                {
+                    if (string.IsNullOrWhiteSpace(line)) continue;
+                    var job = JsonSerializer.Deserialize<Job>(line)!;
+                    jobs.Add(job);
+                }
+            }
+            return jobs;
+        }
+    }
+}
